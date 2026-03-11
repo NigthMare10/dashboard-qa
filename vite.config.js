5e0416a28667182f5c539426d95e19c165936f94
NigthMare <cesar.vivas@GRUPOELCORSO.local>
2026-03-11 10:50:16 -0600

    Final cleanup of vite config and base path for GH Pages

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/dashboard-qa/',
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-      },
-    },
-  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
